7ea0725e0e1d1a55783a075d105222b525b55766
Daniel Rowe <daniel@drowe.dev>
2021-05-14 20:43:11 +0000

    initial commit

---
+++ final.py
@@ -0,0 +1,26 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # Analyzing State Vaccination Rate and 2020 Election Results
+#
+# Daniel Rowe
+# %% [markdown]
+# ## Table of Contents
+# - [1. Introduction](#1-introduction)
+# - [2. Set Up](#2-set-up)
+# - [3. Data Wrangling](#3-data-wrangling)
+# - [4. Exploratory Data Analysis](#4-exploratory-data-analysis)
+# - [5. Hypothesis Testing and Machine Learning](#5-hypothesis-testing-and-machine-learning)
+# - [6. Conclusion](#6-conclusion)
+# %% [markdown]
+# ## 1. Introduction
+# %% [markdown]
+# ## 2. Set Up
+# %% [markdown]
+# ## 3. Data Wrangling
+# %% [markdown]
+# ## 4. Exploratory Data Analysis
+# %% [markdown]
+# ## 5. Hypothesis Testing and Machine Learning
+# %% [markdown]
+# ## 6. Conclusion
